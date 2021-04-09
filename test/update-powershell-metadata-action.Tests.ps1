9557d605768165a88dfc788b976567b5692dfc40
Nate Scherer <376408+natescherer@users.noreply.github.com>
2021-04-09 17:36:46 -0400

    Add initial test structure

---
+++ test/update-powershell-metadata-action.Tests.ps1
@@ -0,0 +1,14 @@
+Describe 'Module Tests' {
+    It 'version' {
+    }
+    It 'copyright' {
+    }
+    It 'description' {
+    }
+    It 'releasenotes' {
+    }
+    It 'functionstoexport' {
+    }
+    It 'functionstoexportpath' {
+    }
+}
