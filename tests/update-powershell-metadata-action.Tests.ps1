d2163d0643fec38945b03a5f1c6291a22163bcfa
Nate Scherer <376408+natescherer@users.noreply.github.com>
2023-08-01 16:15:35 -0400

    Fix script tests

--- tests/update-powershell-metadata-action.Tests.ps1
+++ tests/update-powershell-metadata-action.Tests.ps1
@@ -60,41 +60,36 @@ Describe 'Script Tests' {
     BeforeAll {
         $Script1Path = Join-Path $env:GITHUB_WORKSPACE "tests" "Test-Script1" "Test-Script1.ps1"
         $Script2Path = Join-Path $env:GITHUB_WORKSPACE "tests" "Test-Script2" "Test-Script2.ps1"
-
-        Write-Host "Script1Info"
-        $Data = Test-PSScriptFileInfo -Path $Script1Path
-        $JsonData = ConvertTo-Json $Data -Depth 100
-        Write-Host $JsonData
     }
 
     It 'version' {
-        $Data = Test-PSScriptFileInfo -Path $Script1Path
+        $Data = Get-PSScriptFileInfo -Path $Script1Path
 
         $Data.ScriptMetadataComment.Version | Should -Be "1.1.1"
     }
     It 'version with prerelease' {
-        $Data = Test-PSScriptFileInfo -Path $Script2Path
+        $Data = Get-PSScriptFileInfo -Path $Script2Path
 
         $Data.ScriptMetadataComment.Version | Should -Be "1.1.1-alpha1"
     }
     It 'copyright static' {
-        $Data = Test-PSScriptFileInfo -Path $Script1Path
+        $Data = Get-PSScriptFileInfo -Path $Script1Path
 
         $Data.ScriptMetadataComment.Copyright | Should -Be "(c) 2001 John Doe. All rights reserved."
     }
     It 'copyright with YYYY' {
-        $Data = Test-PSScriptFileInfo -Path $Script2Path
+        $Data = Get-PSScriptFileInfo -Path $Script2Path
 
         $CurrentYear = Get-Date -Format "yyyy"
         $Data.ScriptMetadataComment.Copyright | Should -Be "(c) $CurrentYear John Doe. All rights reserved." 
     }
     It 'description' {
-        $Data = Test-PSScriptFileInfo -Path $Script1Path
+        $Data = Get-PSScriptFileInfo -Path $Script1Path
 
         $Data.ScriptHelpComment.Description | Should -Be "This is a test script."
     }
     It 'releaseNotes' {
-        $Data = Test-PSScriptFileInfo -Path $Script1Path
+        $Data = Get-PSScriptFileInfo -Path $Script1Path
 
         $Data.ScriptMetadataComment.ReleaseNotes | Should -Be "Stuff changed."
     }
